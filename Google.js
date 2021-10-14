e572d54a487635d5b334fc417344917490c5856c
Stephen O'Leary <s.james.oleary@gmail.com>
2021-10-14 16:44:02 -0600

    Change input format for directions start and end

--- Google.js
+++ Google.js
@@ -25,68 +25,57 @@ const { GoogleSession: Session } = require("./Session"),
       catcher = require("./catcher");
 require("dotenv").config();
 
-module.exports = class Google {
+class Google {
+  /**
+   * Creates a new Session
+   * @constructor
+   * @param {string} endpointPath The path of the endpoint to be used for requests to the Google Api
+   */
+  constructor(endpointPath) {
+    this.endpointPath = endpointPath;
+    this.key = process.env.GOOGLE_API_KEY;
+  }
+
+  /**
+   * Calls an endpoint of the Google Maps API
+   * @param {Object.<string, string>} queryParams
+   * @param {Object} [options] 
+   * @param {boolean} options.isSession
+   * @returns {Promise<Object|TypeError|Error>}
+   */
+  async getEndpoint(queryParams, options = {}) {
+    const { isSession = false } = options;
+    if (typeof queryParams !== "object" || typeof isSession !== "boolean") throw new TypeError("Invalid Argument");
+    const params = new URLSearchParams(queryParams);
+    params.append("key", this.key);
+    if (isSession) params.append("sessiontoken", this.session.token);
+    const path = `${process.env.GOOGLE_API_URL}${this.endpointPath}?${params.toString()}`;
+    let response = await axios.get(path);
+    return response.data;
+  }
+}
+
+class GoogleSearch extends Google {
   /**
    * Creates a new Session
    * @constructor
    * @param {string} [token] (Optional) A unique token for the session
    */
   constructor(token) {
+    super("place/autocomplete/json");
     this.session = new Session(token);
-    this.key = process.env.GOOGLE_API_KEY;
-  }
-
-  /**
-   * Calls an endpoint of the Google Maps API
-   * @param {string} route 
-   * @param {Object} [options] 
-   * @param {Object.<string, string>} options.queryParams
-   * @param {boolean} options.isSession
-   * @returns {Promise}
-   */
-  async getEndpoint(route, options = {}) {
-    const { queryParams = {}, isSession = true } = options;
-    if (typeof route !== "string" || typeof queryParams !== "object" || typeof isSession !== "boolean") throw new TypeError("Invalid Argument");
-    const params = new URLSearchParams(queryParams);
-    params.append("key", this.key);
-    if (isSession) params.append("sessiontoken", this.session.token);
-    const path = `${process.env.GOOGLE_API_URL}${route}?${params.toString()}`;
-    let response = await axios.get(path);
-    return response.data;
   }
 
   /**
    * Calls the place autocomplete endpoint of the Google Maps API
    * @param {string} query 
-   * @returns {Promise<AutocompleteResult>} {@link AutocompleteResult}
+   * @returns {Promise<AutocompleteResult|TypeError|Error>} {@link AutocompleteResult}
    */
-  async autocomplete(query) {
+   async autocomplete(query) {
     if (typeof query !== "string") throw new TypeError("Invalid Argument");
-    let response = await this.getEndpoint("place/autocomplete/json", { queryParams: { input: query } });
+    let response = await this.getEndpoint({ input: query }, { isSession: true });
     if (response.status !== "OK") throw new Error(response.status);
-    return Google.formatAutocomplete(response.predictions);
-  }
-
-  /**
-   * Calls the directions endpoint of the Google Maps API
-   * @param {string} start 
-   * @param {string} end 
-   * @param {Object} [options]
-   * @param {string} [options.stops]
-   * @returns {Promise<DirectionsResult>} {@link DirectionsResult}
-   */
-  async directions(start, end, options = {}) {
-    if (typeof start !== "string" || typeof end !== "string") throw new TypeError("Invalid Argument");
-    const { stops, ...otherOptions } = options;
-    let params = {
-      origin: start,
-      destination: end
-    };
-    if (stops && typeof stops === "string") params.waypoints = `optimize:true|${stops}`;
-    Object.assign(params, otherOptions);
-    let response = await this.getEndpoint("directions/json", { queryParams: params, isSession: false });
-    if (response.status !== "OK") throw new Error(response.status);
-    return Google.formatDirections(response.routes[0]);
+    return GoogleSearch.formatResponse(response.predictions);
   }
 
   /**
@@ -94,8 +83,9 @@ module.exports = class Google {
    * @static
    * @param {*[]} [response] 
    * @returns {AutocompleteResult[]} {@link AutocompleteResult}
+   * @throws {TypeError} Invalid Argument
    */
-  static formatAutocomplete(response = []) {
+   static formatResponse(response = []) {
     if (!Array.isArray(response)) throw new TypeError("Invalid Argument");
     return response.filter(prediction => (
       prediction.place_id && prediction.description
@@ -106,14 +96,113 @@ module.exports = class Google {
       secondaryText: prediction.structured_formatting?.secondary_text || undefined
     }));
   }
+}
+
+class GoogleDirections extends Google {
+  /**
+   * Creates a new instance of the GoogleDirections class
+   * @constructor
+   */
+  constructor() {
+    super("directions/json");
+    /** @type {{ id: number, value: string, modifier?: string }[]} */
+    this.stops;
+  }
+
+  get start() {
+    return this.findStop("modifier", "start") || this.findStop("modifier", "end") || this.stops[0];
+  }
+
+  get wasStartSent() {
+    return !!this.findStop("modifier", "start");
+  }
+
+  get end() {
+    return this.findStop("modifier", "end") || this.findStop("modifier", "start") || this.stops[0];
+  }
+
+  get wasEndSent() {
+    return !!this.findStop("modifier", "end");
+  }
+
+  get middleStops() {
+    let middleStops = this.stops.filter(stop => !(stop.modifier === "start" || stop.modifier === "end"));
+    if (middleStops.length === 0) return [];
+    if (!this.wasStartSent && !this.wasEndSent) middleStops.shift();
+    return middleStops;
+  }
+
+  /**
+   * Searches an array of stops for a given query
+   * @param {string} propertyName The property of the stop to search for the query in
+   * @param {string} query The query to search for
+   * @returns {{ id: number, value: string, modifier?: string }|false} The first matching stop; or false if no stop matched the query
+   */
+  findStop(propertyName, query) {
+    let stop = this.stops.find(stop => (stop[propertyName]?.includes(query) || stop[propertyName] === query));
+    return stop || false;
+  }
+
+  // start
+  //  end
+  //   start = start; end = end; Remove start and end from stops
+  //  !end
+  //   start = start; end = start; Remove start from stops; Remove last leg
+  // !start
+  //  end
+  //   start = end; end = end; Remove end from stops; Remove first leg
+  //  !end
+  //   start = stops[0]; end = stops[0]; Remove stops[0] from stops; Remove last leg
+
+  /**
+   * Calls the directions endpoint of the Google Maps API
+   * @param {string} stops
+   * @param {Object} [options]
+   * @returns {Promise<DirectionsResult|TypeError|Error>} {@link DirectionsResult}
+   */
+  async directions(stops, options = {}) {
+    if (typeof stops !== "string" || typeof options !== "object") throw new TypeError("Invalid Argument");
+    // Convert stops string to an array of objects
+    this.stops = stops.split("|").map((stop, index) => {
+      return { 
+        id: index, 
+        ...(stop.includes(":")) ? { 
+          value: stop.split(":")[1], 
+          modifier: stop.split(":")[0] 
+        } : { 
+          value: stop 
+        } 
+      };
+    });
+    let response = await this.getEndpoint({
+      origin: this.start.value,
+      destination: this.end.value,
+      ...(this.middleStops.length > 0) ? { 
+        waypoints: `optimize:true|${this.middleStops.map(stop => stop.value).join("|")}` 
+      } : {},
+      ...options
+    }, { isSession: false });
+    if (response.status !== "OK") throw new Error(response.status);
+    if (!this.wasStartSent && !this.wasEndSent) response.routes[0].legs.pop();
+    if (!this.wasStartSent && this.wasEndSent) response.routes[0].legs.shift();
+    if (!this.wasEndSent && this.wasStartSent) response.routes[0].legs.pop();
+    let stopOrder = (this.wasStartSent || (!this.wasStartSent && !this.wasEndSent)) ? [ this.start.id ] : [];
+    for (const waypointIndex of response.routes[0].waypoint_order) {
+      stopOrder.push(Object.values(this.middleStops).map(stop => stop.id)[waypointIndex]);
+    }
+    if (this.wasEndSent) stopOrder.push(this.end.id);
+    response.routes[0].waypoint_order = stopOrder;
+    return GoogleDirections.formatResponse(response.routes[0]);
+  }
 
   /**
    * Formats the response object
    * @static
    * @param {{}} [route] 
    * @returns {DirectionsResult} {@link DirectionsResult}
+   * @throws {TypeError} Invalid Argument
    */
-  static formatDirections(route = {}) {
+  static formatResponse(route = {}) {
     if (typeof route !== "object") throw new TypeError("Invalid Argument");
     if (Object.keys(route).length === 0) return {};
     return {
@@ -128,4 +217,10 @@ module.exports = class Google {
       }))
     }
   }
+}
+
+module.exports = {
+  Google,
+  GoogleSearch,
+  GoogleDirections
 }
--- routes/directions.js
+++ routes/directions.js
@@ -1,7 +1,8 @@
 const GoogleMaps = require("../Google");
 
 const express = require("express"),
-      router = express.Router();
+      router = express.Router(),
+      sanitize = require("sanitize")();
 
 /**
  * @openapi
@@ -9,27 +10,13 @@ const express = require("express"),
  *   get:
  *     description: Gets directions for a given set of stops
  *     parameters:
- *       - name: start
- *         in: query
- *         required: true
- *         description: Start location for the route. Use a Google place_id when available. Other accepted formats are an address or coordinates. This stop will not be reordered when optimizing the route.
- *         schema:
- *           type: string
- *           example: 1676 40th Street,Calgary,AB
- *       - name: end
- *         in: query
- *         required: true
- *         description: End location for the route. Use a Google place_id when available. Other accepted formats are an address or coordinates. This stop will not be reordered when optimizing the route.
- *         schema:
- *           type: string
- *           example: 785 7th Ave,Calgary,AB
  *       - name: stops
  *         in: query
- *         required: false
- *         description: Stops along the route separated by a "|" character. Use a Google place_id when available. Other accepted formats are an address or coordinates. These stops will be reordered to optimize the route.
+ *         required: true
+ *         description: Stops along the route separated by a "|" character. Use a Google place_id when available. Other accepted formats are an address or coordinates. These stops will be reordered to optimize the route. To specify a start or end point, add the prefix "start:" or "end:" before the stop
  *         schema:
  *           type: string
- *           example: 3368 Heritage Drive,Calgary,AB|235 Heritage Drive,Calgary,AB|1956 Fourth Avenue,Calgary,AB
+ *           example: start:1676 40th Street,Calgary,AB|3368 Heritage Drive,Calgary,AB|235 Heritage Drive,Calgary,AB|1956 Fourth Avenue,Calgary,AB|end:785 7th Ave,Calgary,AB
  *     responses:
  *       '200':
  *         description: Successfully returned a route
@@ -65,17 +52,16 @@ const express = require("express"),
  *               $ref: "#/components/schemas/GeneralError"
  */
 router.get("/", async (req, res) => {
-  const { start, end, ...otherQueries } = req.query;
-  if (typeof start !== "string" || typeof end !== "string") return res.status(400).send({"message": "INVALID_REQUEST"});
+  const { stops, ...otherQueries } = req.query;
+  if (typeof stops !== "string") return res.status(400).send({"message": "INVALID_REQUEST"});
   let googleMaps = new GoogleMaps();
-  let directions;
   try {
-    directions = await googleMaps.directions(start, end, otherQueries);
+    let directions = await googleMaps.directions(stops, otherQueries);
+    return res.status(200).json(directions);
   } catch (err) {
     console.error(err);
     return res.status(500).send({"message": err.message});
   }
-  res.status(200).json(directions);
 });
 
 module.exports = router;
