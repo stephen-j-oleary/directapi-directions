276220f54b678bc395cf7b5f1118763f52266349
stephen-j-oleary <s.james.oleary@gmail.com>
2022-06-20 19:18:01 -0600

    Add main endpoint handler Routes requests to the expected endpoint handlers

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+
+import express from "express";
+import docs from "./docs.js";
+import authcode from "./authcode.js";
+import authorize from "./authorize.js";
+import token from "./token.js";
+import user from "./user.js";
+import client from "./client.js";
+
+const router = express.Router();
+
+
+// Automatically generated documentation
+router.use("/docs", docs);
+
+// Api routes
+router.use("/authcode", authcode);
+
+router.use("/authorize", authorize);
+
+router.use("/token", token);
+
+router.use("/user", user);
+
+router.use("/client", client);
+
+
+export default router;
