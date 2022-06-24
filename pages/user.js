b9d8969ad8fe0c8615640da00b40469a2f11af59
stephen-j-oleary <s.james.oleary@gmail.com>
2022-06-23 23:31:23 -0600

    Change to new validator middleware

--- pages/user.js
+++ pages/user.js
@@ -6,13 +6,14 @@ import express from "express";
 import jsonPatch from "json-patch";
 import mergePatch from "json-merge-patch";
 
-import validateParams from "../../helpers/validate-params.mjs";
 import assignDefined from "../../helpers/assignDefined.mjs";
 import ApiError from "../../helpers/ApiError.mjs";
 import User from "../schemas/User.js";
+import schemas from "../schemas/requests/user.js";
 
 // Middleware
 import authorizer, { basic, client_basic, access_token } from "../middleware/authorizer.js";
+import validator from "../middleware/validator.js";
 
 const router = express.Router();
 
@@ -52,29 +53,31 @@ const router = express.Router();
  *         description: Server error
  *         $ref: "#/components/responses/ApiError"
  */
-router.get("/", validateParams({
-  user_id: { in: "query", type: "string" }
-}), authorizer([basic, access_token], "user:read"), async (req, res, next) => {
-  // Request parameters
-  const { authorized_user_id } = res.locals;
-  const { user_id } = req.query;
+router.get("/",
+  validator(schemas.get),
+  authorizer([basic, access_token], "user:read"),
+  async (req, res, next) => {
+    // Request parameters
+    const { authorized_user_id } = res.locals;
+    const { user_id } = req.query;
 
-  try {
-    const queryFilter = assignDefined({}, { user_id });
-    const authFilter = { user_id: authorized_user_id };
-    const filter = {
-      $and: [ queryFilter, authFilter ]
-    };
+    try {
+      const queryFilter = assignDefined({}, { user_id });
+      const authFilter = { user_id: authorized_user_id };
+      const filter = {
+        $and: [ queryFilter, authFilter ]
+      };
 
-    const users = await User.find(filter, { password: 0 });
-    if (!users || users.length === 0) throw new ApiError(404, "resource_not_found");
+      const users = await User.find(filter, { password: 0 });
+      if (!users || users.length === 0) throw new ApiError(404, "resource_not_found");
 
-    res.status(200).json(users);
+      res.status(200).json(users);
+    }
+    catch (err) {
+      next(err);
+    }
   }
-  catch (err) {
-    next(err);
-  }
-});
+);
 
 
 /**
@@ -110,22 +113,27 @@ router.get("/", validateParams({
  *         description: Server error
  *         $ref: "#/components/responses/ApiError"
  */
-router.post("/", authorizer(client_basic), async (req, res, next) => {
-  const { email, password, name } = req.body;
+router.post("/",
+  validator(schemas.post),
+  authorizer(client_basic),
+  async (req, res, next) => {
+    const { email, password, name } = req.body;
 
-  try {
-    // Create the new user document
-    const user = new User({ email, password, name });
-    await user.save();
+    try {
+      // Create the new user document
+      const user = new User({ email, password, name });
+      await user.save();
 
-    const { password: _, ...response } = user.toObject();
+      const { password: _, ...response } = user.toObject();
 
-    res.status(201).json(response);
+      res.status(201).json(response);
+    }
+    catch (err) {
+      next(err);
+    }
   }
-  catch (err) {
-    next(err);
-  }
-});
+);
+
 
 /**
  * @openapi
@@ -173,40 +181,42 @@ router.post("/", authorizer(client_basic), async (req, res, next) => {
  *         description: Server error
  *         $ref: "#/components/responses/ApiError"
  */
-router.patch("/:user_id?", validateParams({
-  user_id: { in: "params", type: "string", required: true }
-}), authorizer([basic, access_token], "user:update"), async (req, res, next) => {
-  const { authorized_user_id } = res.locals;
-  const { user_id } = req.params;
-
-  try {
-    const queryFilter = { user_id };
-    const authFilter = { user_id: authorized_user_id };
-    const filter = {
-      $and: [ queryFilter, authFilter ]
-    };
-
-    let user = await User.findOne(filter);
-    if (!user) throw new ApiError(404, "resource_not_found", "User could not be found");
+router.patch("/:user_id",
+  validator(schemas.patch),
+  authorizer([basic, access_token], "user:update"),
+  async (req, res, next) => {
+    const { authorized_user_id } = res.locals;
+    const { user_id } = req.params;
 
     try {
-      if (req.is("application/json-patch+json")) jsonPatch.apply(user, req.body);
-      if (req.is("application/merge-patch+json")) user = mergePatch.apply(user, req.body);
+      const queryFilter = { user_id };
+      const authFilter = { user_id: authorized_user_id };
+      const filter = {
+        $and: [ queryFilter, authFilter ]
+      };
+
+      let user = await User.findOne(filter);
+      if (!user) throw new ApiError(404, "resource_not_found", "User could not be found");
+
+      try {
+        if (req.is("application/json-patch+json")) jsonPatch.apply(user, req.body);
+        if (req.is("application/merge-patch+json")) user = mergePatch.apply(user, req.body);
+      }
+      catch (err) {
+        throw new ApiError(400, "invalid_request", err.message);
+      }
+
+      await User.replaceOne(filter, user);
+
+      const { password: _, ...response } = user.toObject();
+
+      res.status(201).json(response);
     }
     catch (err) {
-      throw new ApiError(400, "invalid_request", err.message);
+      next(err);
     }
-
-    await User.replaceOne(filter, user);
-
-    const { password: _, ...response } = user.toObject();
-
-    res.status(201).json(response);
   }
-  catch (err) {
-    next(err);
-  }
-});
+);
 
 
 /**
@@ -249,28 +259,30 @@ router.patch("/:user_id?", validateParams({
  *         description: Server error
  *         $ref: "#/components/responses/ApiError"
  */
-router.delete("/:user_id?", validateParams({
-  user_id: { in: "params", type: "string", required: true }
-}), authorizer([basic, access_token], "user:delete"), async (req, res, next) => {
-  const { authorized_user_id } = res.locals;
-  const { user_id } = req.params;
+router.delete("/:user_id",
+  validator(schemas.delete),
+  authorizer([basic, access_token], "user:delete"),
+  async (req, res, next) => {
+    const { authorized_user_id } = res.locals;
+    const { user_id } = req.params;
 
-  try {
-    const queryFilter = { user_id };
-    const authFilter = { user_id: authorized_user_id };
-    const filter = {
-      $and: [ queryFilter, authFilter ]
-    };
+    try {
+      const queryFilter = { user_id };
+      const authFilter = { user_id: authorized_user_id };
+      const filter = {
+        $and: [ queryFilter, authFilter ]
+      };
 
-    const { deletedCount } = await User.deleteOne(filter);
-    if (deletedCount === 0) throw new ApiError(404, "resource_not_found");
+      const { deletedCount } = await User.deleteOne(filter);
+      if (deletedCount === 0) throw new ApiError(404, "resource_not_found");
 
-    res.status(201).json({ user_id });
+      res.status(201).json({ user_id });
+    }
+    catch (err) {
+      next(err);
+    }
   }
-  catch (err) {
-    next(err);
-  }
-});
+);
 
 
 router.use("/verify", verify);
