d6dfccbdf95655e0a81d8da74746ffc7e56a3817
stephen-j-oleary <s.james.oleary@gmail.com>
2022-08-31 16:09:04 -0600

    Refactor directions endpoint to use new directions class

--- routes/directions.js
+++ routes/directions.js
@@ -1,4 +1,4 @@
-const { GoogleDirections } = require("../Google");
+const Directions = require("../Directions.js");
 
 const express = require("express"),
       router = express.Router();
@@ -12,54 +12,59 @@ const express = require("express"),
  *       - name: stops
  *         in: query
  *         required: true
- *         description: Stops along the route separated by a "|" character. Use a Google place_id when available. Other accepted formats are an address or coordinates. These stops will be reordered to optimize the route. Stop modifiers can be added to the start of each stop separated by a ":" character.
+ *         description: A string of pipe(|) separated stops. Different modifiers can be added before the address separated by a colon(:). Use a Google place_id when available. Other accepted formats are an address or coordinates.
  *         schema:
  *           type: string
  *           example: start:1676 40th Street,Calgary,AB|3368 Heritage Drive,Calgary,AB|235 Heritage Drive,Calgary,AB|1956 Fourth Avenue,Calgary,AB|end:785 7th Ave,Calgary,AB
  *     responses:
- *       '200':
+ *       "200":
  *         description: Successfully returned a route
- *         content: 
+ *         content:
  *           application/json:
  *             schema:
  *               type: array
- *               items: 
+ *               items:
  *                 $ref: "#/components/schemas/DirectionsResult"
- *       '400':
+ *       "400":
  *         description: Invalid Request
- *         content: 
+ *         content:
  *           application/json:
  *             schema:
  *               $ref: "#/components/schemas/GeneralError"
- *       '401':
+ *       "401":
  *         description: Invalid Authentication
- *         content: 
+ *         content:
  *           application/json:
  *             schema:
  *               $ref: "#/components/schemas/GeneralError"
- *       '403':
+ *       "403":
  *         description: Forbidden
- *         content: 
+ *         content:
  *           application/json:
  *             schema:
  *               $ref: "#/components/schemas/GeneralError"
- *       '500':
+ *       "500":
  *         description: Internal Error
- *         content: 
+ *         content:
  *           application/json:
  *             schema:
  *               $ref: "#/components/schemas/GeneralError"
  */
 router.get("/", async (req, res) => {
-  const { stops, ...otherQueries } = req.query;
-  if (typeof stops !== "string") return res.status(400).json({code: "invalid_request", message: "Invalid Request"});
-  let googleMaps = new GoogleDirections();
+  const { stops } = req.query;
+
+  if (typeof stops !== "string") return res.status(400).json({ code: "invalid_request", message: "Invalid request" });
+
+  const directions = new Directions();
+
   try {
-    let directions = await googleMaps.directions(stops, otherQueries);
-    return res.status(200).json(directions);
-  } catch (err) {
-    if (err.message === "Too Few Stops") return res.status(400).json({code: "invalid_request", message: "Too Few Stops"});
-    return res.status(500).json({"message": err.message});
+    directions.setStops(Directions.parseStops(stops));
+    await directions.calculate();
+    return res.status(200).json(directions.response);
+  }
+  catch (err) {
+    if (err.message === "Too few stops") return res.status(400).json({ code: "invalid_request", message: "Too Few Stops" });
+    return res.status(500).json({ message: err.message });
   }
 });
 
