b32efe1bfc9fb6af305eeccdcce86fb433ba275f
stephen-j-oleary <s.james.oleary@gmail.com>
2022-08-31 16:08:19 -0600

    Rapid api authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+
+module.exports = (req, res, next) => {
+  const proxySecret = req.header("X-RapidAPI-Proxy-Secret");
+
+  return (proxySecret && proxySecret === process.env.RAPIDAPI_PROXY_SECRET)
+    ? next()
+    : res.status(401).json({ code: "invalid_client", message: "Requests must come from the RapidAPI server" })
+}
