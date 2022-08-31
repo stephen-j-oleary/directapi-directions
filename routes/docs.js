7dd868f4b197e13baf8a311d6971fb6b99b09cd3
stephen-j-oleary <s.james.oleary@gmail.com>
2022-08-31 16:44:24 -0600

    Remove outdated security schemas

--- routes/docs.js
+++ routes/docs.js
@@ -12,43 +12,19 @@ const apiDefinition = {
     title: "Plan My Trip",
     description: "Plan My Trip is an API that can be used to optimize delivery routes or any other trip with multiple stops. It optimizes the order of the stops and returns each section (leg) of the trip."
   },
-  servers: [{url: process.env.API_URL}],
-  components: {
-    securitySchemes: {
-      oAuth2: {
-        type: "oauth2",
-        flows: {
-          clientCredentials: {
-            tokenUrl: `${process.env.API_URL}/authentication/token`,
-            scopes: {
-              "free": "See the plans page for more information about the free tier",
-              "tier1": "See the plans page for more information about tier 1",
-              "tier2": "See the plans page for more information about tier 2"
-            }
-          }
-        }
-      },
-      basicAuth: {
-        type: "http",
-        scheme: "basic"
-      }
-    }
-  },
-  security: [{
-    oAuth2: []
-  }]
+  servers: [{url: process.env.API_URL}]
 };
 const endpointSpecs = swaggerJSDoc({
   definition: apiDefinition,
-  apis: ['./routes/**/*.js'],
+  apis: ["./routes/**/*.js"],
 });
 const generateComponents = () => new Promise(resolve => {
   let instance = jsdocSwagger(router)({
     ...apiDefinition,
     baseDir: __dirname,
-    filesPattern: '../**/*.js',
+    filesPattern: "../**/*.js",
     exposeSwaggerUI: false,
-    apiDocsPath: '/doc-components',
+    apiDocsPath: "/doc-components",
     notRequiredAsNullable: true
   });
   instance.on("finish", data => {
