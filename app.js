e2b8bfb53985d73d8cd0854e7a6e7db83221bce6
stephen-j-oleary <s.james.oleary@gmail.com>
2022-08-31 16:02:24 -0600

    Refactor app code

--- app.js
+++ app.js
@@ -1,40 +1,27 @@
 /**
  * @typedef {Object} GeneralError
- * @property {string} code - enum:invalid_request, invalid_client, invalid_grant, unauthorized_client, unsupported_grant_type, invalid_scope
+ * @property {string} code
  * @property {string} message.required
  */
 
 const express = require("express"),
-      cors = require("cors"),
-      bodyParser = require("body-parser");
+      cors = require("cors");
 require("dotenv").config();
 
 const app = express(),
       port = process.env.PORT;
 
-app.use("/docs", require("./routes/docs")); // Automatically generated documentation
-
-app.use(bodyParser.json()); // Parse application/json request body
+app.use(express.json()); // Parse application/json request body
 
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use("/", require("./routes/authentication")); // Run authentiaction middleware
-
-app.use("/search", require("./routes/search"));
-
-app.use("/directions", require("./routes/directions"));
-
-app.use("/", (req, res) => {
-  res.json({
-    description: "This is the main API route",
-    locals: res.locals
-  });
-});
+// API Endpoints
+app.use("/", require("./routes"));
 
 if (!module.parent) {
   app.listen(port, () => {
