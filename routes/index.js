200421782d9c27846e3a0467587e5487e682404a
stephen-j-oleary <s.james.oleary@gmail.com>
2022-10-05 17:46:47 -0600

    Move routes file to its own folder

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 
 import { Router } from "express";
-import authentication from "./controllers/authentication.js";
-import directions from "./controllers/directions.js";
+import authentication from "../controllers/authentication.js";
+import directions from "../controllers/directions.js";
 
 const router = Router();
 
