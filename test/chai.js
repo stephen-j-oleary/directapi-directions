f57546e4c05a7e163c1283c506c9f9316abd4869
stephen-j-oleary <s.james.oleary@gmail.com>
2022-10-04 20:41:30 -0600

    Add chai json schema module

--- test/chai.js
+++ test/chai.js
@@ -5,10 +5,12 @@ import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import sinonChai from "sinon-chai";
 import chaiHttp from "chai-http";
+import chaiJsonSchema from "chai-json-schema";
 
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 chai.use(chaiHttp);
+chai.use(chaiJsonSchema);
 
 export const { expect } = chai;
 
