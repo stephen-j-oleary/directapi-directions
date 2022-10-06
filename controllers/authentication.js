658ec75e6c1f121840636d73246f16ead5dbbc3c
stephen-j-oleary <s.james.oleary@gmail.com>
2022-10-05 23:57:43 -0600

    Change interface to allow for global error handling

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,16 @@
 
-export default (req, res, next) => {
+export class AuthError extends Error {
+  constructor(code, message, data) {
+    super(message);
+    this.code = code;
+    this.data = data;
+  }
+}
+
+export default (req, _, next) => {
   const proxySecret = req.header("X-RapidAPI-Proxy-Secret");
 
   return (proxySecret && proxySecret === process.env.RAPIDAPI_PROXY_SECRET)
     ? next()
-    : res.status(401).json({ code: "invalid_client", message: "Requests must come from the RapidAPI server" })
+    : next(new AuthError("invalid_client", "Requests must come from the RapidAPI server"));
 }
