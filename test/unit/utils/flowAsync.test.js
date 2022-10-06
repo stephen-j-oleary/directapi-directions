1e6655a42e368d5a0422d38d4ceba2bd47163f4a
stephen-j-oleary <s.james.oleary@gmail.com>
2022-10-06 00:55:06 -0600

    Add missing test case and refactor test suite

--- test/unit/utils/flowAsync.test.js
+++ test/unit/utils/flowAsync.test.js
@@ -4,7 +4,7 @@ import sinon from "sinon";
 import flowAsync from "../../../utils/flowAsync.js";
 
 describe("module flowAsync", () => {
-  let flow, result, fake1, fake2, fake3;
+  let flow, fake1, fake2, fake3;
   const expectedResult = 4;
 
   beforeEach(async () => {
@@ -16,24 +16,30 @@ describe("module flowAsync", () => {
       fake2,
       fake3
     );
-
-    result = await flow(1);
   })
   afterEach(sinon.restore)
 
-  it("should create a pipe", () => {
+  it("should create a pipe", async () => {
+    const result = await flow(1);
+
     return expect(flow).to.be.a("function");
   })
 
-  it("should call functions in order", () => {
+  it("should call functions in order", async () => {
+    const result = await flow(1);
+
     return sinon.assert.callOrder(fake1, fake2, fake3);
   })
 
-  it("should await any async functions", () => {
+  it("should await any async functions", async () => {
+    const result = await flow(1);
+
     return expect(fake3).to.be.calledWith(3);
   })
 
-  it("should have expected result after pipe is executed", () => {
+  it("should have expected result after pipe is executed", async () => {
+    const result = await flow(1);
+
     return expect(result).to.equal(expectedResult);
   })
 
@@ -53,5 +59,16 @@ describe("module flowAsync", () => {
     });
 
     return expect(flow(1)).to.be.rejectedWith("Error message");
+  })
+
+  it("should stop chain of calls if any function rejects", async () => {
+    const message = "Error message";
+    fake2.callsFake(async () => {
+      throw message;
+    });
+
+    await flow(1).catch(() => {});
+
+    return expect(fake3).to.not.be.called;
   })
 })
