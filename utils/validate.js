94835263bd62df3de67adb2db7f27c72300048e6
stephen-j-oleary <s.james.oleary@gmail.com>
2022-10-13 14:48:45 -0600

    Add options to validator

--- utils/validate.js
+++ utils/validate.js
@@ -2,7 +2,7 @@
 import { Validator } from "express-json-validator-middleware";
 import ajvErrors from "ajv-errors";
 
-const { validate } = new Validator();
+const { validate } = new Validator({ allErrors: true });
 ajvErrors(validate.ajv);
 
 export default validate;
