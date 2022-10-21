a3a4c114f49f62c07470ebee7e0157e5c80e76d5
stephen-j-oleary <s.james.oleary@gmail.com>
2022-10-21 12:36:51 -0600

    Fix stops service tests

--- test/unit/Stop.test.js
+++ test/unit/Stop.test.js
@@ -3,103 +3,154 @@ import { expect } from "../chai.js";
 import Stop from "../../services/Stop.js";
 
 describe("class Stop", () => {
-  it("should have expected properties", done => {
-    const address = "adress1";
-    const modifiers = ["start"];
+  it("should handle object argument", () => {
+    const props = { address: "value" };
 
-    const stop = new Stop({ address, modifiers });
+    const stop = new Stop(props);
 
-    expect(stop).to.have.property("address").that.is.a("string");
-    expect(stop).to.have.property("modifiers").that.is.an("array");
-    done();
+    return expect(stop).to.be.an.instanceOf(Stop).that.deep.includes(props);
   })
 
-  it("should have expected default properties", done => {
-    const expectedDefaultAddress = "";
-    const expectedDefaultModifiers = [];
+  it("should handle missing argument", () => {
+    const stop = new Stop();
+
+    return expect(stop).to.be.an.instanceOf(Stop);
+  })
+
+  it("should handle invalid address prop", () => {
+    const expectedVal = "";
+
+    const stop = new Stop({ address: [] });
+
+    return expect(stop.address).to.equal(expectedVal);
+  })
+
+  it("should handle modifier with only key", () => {
+    const expectedVal = { key: undefined };
+
+    const stop = new Stop({ modifiers: "key" });
+
+    return expect(stop.modifiers).to.deep.equal(expectedVal);
+  })
+
+  it("should handle invalid modifiers prop", () => {
+    const expectedVal = {};
+
+    const stop = new Stop({ modifiers: false });
+
+    return expect(stop.modifiers).to.deep.equal(expectedVal);
+  })
+
+  it("should handle combining modifiers from address string and modifiers prop", () => {
+    const expectedModifiers = { a: "0", b: "2", c: "3" };
+    const props = {
+      address: "a:0;b:1;address",
+      modifiers: { b: "2", c: "3" }
+    };
+
+    const stop = new Stop(props);
+
+    return expect(stop.modifiers).to.deep.equal(expectedModifiers);
+  })
+
+  it("should have expected default properties", () => {
+    const expectedProps = {
+      address: "",
+      modifiers: {}
+    };
 
     const stop = new Stop();
 
-    expect(stop.address).to.equal(expectedDefaultAddress);
-    expect(stop.modifiers).to.deep.equal(expectedDefaultModifiers);
-    done();
+    return expect(stop).to.deep.include(expectedProps);
   })
 
-  it("should handle string initializer with modifiers", done => {
-    const string = "type:origin;andOther:modifiers;address";
+  it("toString should handle stop with no modifiers", () => {
+    const expectedStr = "address";
+
+    const str = new Stop({ address: "address" })
+      .toString();
+
+    return expect(str).to.equal(expectedStr);
+  })
+
+  it("toString should handle stop one modifier", () => {
+    const expectedStr = "address";
+
+    const str = new Stop({ address: "address", modifiers: { a: "1" } })
+      .toString();
+
+    return expect(str).to.equal(expectedStr);
+  })
+
+  it("toString should handle stop with multiple modifiers", () => {
+    const expectedStr = "address";
+
+    const str = new Stop({ address: "address", modifiers: { a: "1", b: "2" } })
+      .toString();
+
+    return expect(str).to.equal(expectedStr);
+  })
+
+  it("toModifierString should handle stop with no modifiers", () => {
+    const expectedStr = "address";
+
+    const str = new Stop({ address: "address" })
+      .toModifierString();
+
+    return expect(str).to.equal(expectedStr);
+  })
+
+  it("toModifierString should handle stop one modifier", () => {
+    const expectedStr = "a:1;address";
+
+    const str = new Stop({ address: "address", modifiers: { a: "1" } })
+      .toModifierString();
+
+    return expect(str).to.equal(expectedStr);
+  })
+
+  it("toModifierString should handle stop with multiple modifiers", done => {
     const expectedAddress = "address";
-    const expectedModifiers = { type: "origin", andOther: "modifiers" };
+    const expectedModifiers = ["a:1", "b:2"];
 
-    const stop = new Stop(string);
+    const str = new Stop({ address: "address", modifiers: { a: "1", b: "2" } })
+      .toModifierString();
 
-    expect(stop.address).to.equal(expectedAddress);
-    expect(stop.modifiers).to.deep.equal(expectedModifiers);
+    expect(str).to.satisfy(val => val.endsWith(expectedAddress));
+    expectedModifiers.forEach(val => expect(str).to.include(val));
     done();
   })
 
-  it("should handle string initializer without modifiers", done => {
-    const string = "address";
-    const expectedAddress = "address";
-    const expectedModifiers = {};
+  it("setModifier should set expected value", () => {
+    const [key, value] = ["a", "1"];
+    const stop = new Stop();
 
-    const stop = new Stop(string);
+    stop.setModifier(key, value);
 
-    expect(stop.address).to.equal(expectedAddress);
-    expect(stop.modifiers).to.deep.equal(expectedModifiers);
-    done();
+    return expect(stop.modifiers).to.have.property(key).that.equals(value);
   })
 
-  describe("method hasModifier", () => {
-    const address = "address1";
-    const modifiersWithOrigin = {
-      type: "origin",
-      andOther: "modifiers"
-    };
-    const modifiersWithoutOrigin = {
-      type: "destination",
-      andOther: "modifiers"
-    };
+  it("hasModifier should return true when modifier is present", () => {
+    const stop = new Stop({ modifiers: { a: "1" } });
 
-    it("should return true when modifier is present", () => {
-      const stop = new Stop({
-        address,
-        modifiers: modifiersWithOrigin
-      });
+    return expect(stop.hasModifier("a")).to.be.true;
+  })
 
-      return expect(stop.hasModifier("type")).to.be.true;
-    })
+  it("hasModifier should return true when modifier is present and value is correct", () => {
+    const stop = new Stop({ modifiers: { a: "1" } });
 
-    it("should return true when modifier is present and value is correct", () => {
-      const stop = new Stop({
-        address,
-        modifiers: modifiersWithOrigin
-      });
+    return expect(stop.hasModifier("a", "1")).to.be.true;
+  })
 
-      return expect(stop.hasModifier("type", "origin")).to.be.true;
-    })
+  it("hasModifier should return false when modifier is not present", () => {
+    const stop = new Stop();
 
-    it("should return false when modifier is not present", () => {
-      const stop = new Stop({
-        address,
-        modifiers: modifiersWithOrigin
-      });
+    return expect(stop.hasModifier("a")).to.be.false;
+  })
 
-      return expect(stop.hasModifier("missing")).to.be.false;
-    })
+  it("hasModifier should return false when modifier is present but value is not correct", () => {
+    const stop = new Stop({ modifiers: { a: "1" } });
 
-    it("should return false when modifier is present but value is not correct", () => {
-      const stop = new Stop({
-        address,
-        modifiers: modifiersWithoutOrigin
-      });
-
-      return expect(stop.hasModifier("type", "origin")).to.be.false;
-    })
-
-    it("should return false when no modifiers are present", () => {
-      const stop = new Stop({ address });
-
-      return expect(stop.hasModifier("type")).to.be.false;
-    })
+    return expect(stop.hasModifier("a", "2")).to.be.false;
   })
 })
--- test/unit/Stops.test.js
+++ test/unit/Stops.test.js
@@ -4,127 +4,273 @@ import Stops from "../../services/Stops.js";
 import Stop from "../../services/Stop.js";
 
 describe("class Stops", () => {
-  it("should have expected properties", () => {
-    const stops = new Stops([
-      new Stop(),
-      new Stop()
-    ]);
+  it("should handle object argument", () => {
+    const props = { stops: [new Stop()] };
 
-    return expect(stops).to.have.property("stops").that.is.an("array");
+    const stops = new Stops(props);
+
+    return expect(stops).to.be.an.instanceOf(Stops).that.has.property("stops");
+  })
+
+  it("should handle missing argument", () => {
+    const stops = new Stops();
+
+    return expect(stops).to.be.an.instanceOf(Stops);
   })
 
   it("should have expected default properties", () => {
-    const expectedDefaultStops = [];
+    const expectedProps = {
+      stops: { value: [], string: "", modifierString: "" },
+      origin: undefined,
+      destination: undefined,
+      waypoints: { value: [], string: "", modifierString: "" }
+    };
 
     const stops = new Stops();
 
-    return expect(stops.stops).to.deep.equal(expectedDefaultStops);
+    return expect(stops).to.deep.include(expectedProps);
   })
 
-  it("should handle string initializer", done => {
-    const string = "origin:address1|address2";
+  it("should handle stops string", done => {
+    const props = { stops: "address1|address2" };
+    const expectedAddresses = ["address1", "address2"];
 
-    const stops = new Stops(string);
+    const stops = new Stops(props);
 
-    expect(stops.stops).to.have.lengthOf(2);
-    stops.stops.forEach(s => expect(s).to.be.instanceOf(Stop));
+    expect(stops.stops.value).to.be.an("array");
+    stops.stops.value.forEach((stop, index) => expect(stop).to.be.an.instanceOf(Stop).that.has.property("address", expectedAddresses[index]));
     done();
-  });
-
-  describe("get origin", () => {
-    const originStop = new Stop("type:origin;address");
-    const destinationStop = new Stop("type:destination;address");
-
-    it("should have the expected properties", done => {
-      const stops = new Stops([new Stop("addr1"), originStop, new Stop("addr2")]);
-
-      expect(stops.origin).to.have.property("index").that.is.a("number");
-      expect(stops.origin).to.have.property("value").that.is.an("object");
-      done();
-    })
-
-    it("should be a stop with origin modifier", () => {
-      const stops = new Stops([new Stop("addr1"), originStop, new Stop("addr2")]);
-
-      return expect(stops.origin.value).to.deep.equal(originStop);
-    })
-
-    it("should return a stop with destination modifier if no origin modifier exists", () => {
-      const stops = new Stops([new Stop("addr1"), destinationStop, new Stop("addr2")]);
-
-      return expect(stops.origin.value).to.deep.equal(destinationStop);
-    })
-
-    it("should return the first stop if no origin or destination modifier exists", () => {
-      const firstStop = new Stop("firstStop");
-      const stops = new Stops([firstStop, new Stop("addr1"), new Stop("addr2")]);
-
-      return expect(stops.origin.value).to.deep.equal(firstStop);
-    })
   })
 
-  describe("get waypoints", () => {
-    const originStop = new Stop("type:origin;address");
-    const destinationStop = new Stop("type:destination;address");
+  it("should handle stops array of plain objects", done => {
+    const props = { stops: [
+      { address: "address1" },
+      { address: "address2" }
+    ] };
+    const expectedAddresses = ["address1", "address2"];
 
-    it("should remove any stops with origin or destination modifiers", () => {
-      const stops = new Stops([new Stop("addr1"), originStop, new Stop("addr2"), destinationStop]);
+    const stops = new Stops(props);
 
-      return expect(stops.waypoints).to.have.lengthOf(2);
-    })
-
-    it("should remove first stop if no origin or destination modifiers exist", () => {
-      const stops = new Stops([new Stop("addr1"), new Stop("addr2"), new Stop("addr3")]);
-
-      return expect(stops.waypoints).to.have.lengthOf(2);
-    })
+    expect(stops.stops.value).to.be.an("array");
+    stops.stops.value.forEach((stop, index) => expect(stop).to.be.an.instanceOf(Stop).that.has.property("address", expectedAddresses[index]));
+    done();
   })
 
-  describe("get destination", () => {
-    const originStop = new Stop("type:origin;address");
-    const destinationStop = new Stop("type:destination;address");
+  it("should handle stops array of Stop instances", done => {
+    const props = { stops: [
+      new Stop({ address: "address1" }),
+      new Stop({ address: "address2" })
+    ] };
+    const expectedAddresses = ["address1", "address2"];
 
-    it("should have the expected properties", done => {
-      const stops = new Stops([new Stop("addr1"), destinationStop, new Stop("addr2")]);
+    const stops = new Stops(props);
 
-      expect(stops.destination).to.have.property("index").that.is.a("number");
-      expect(stops.destination).to.have.property("value").that.is.an("object");
-      done();
-    })
-
-    it("should return a stop with destination modifier", () => {
-      const stops = new Stops([new Stop("addr1"), destinationStop, new Stop("addr2")]);
-
-      return expect(stops.destination.value).to.deep.equal(destinationStop);
-    })
-
-    it("should return a stop with origin modifier if no destination modifier exists", () => {
-      const stops = new Stops([new Stop("addr1"), originStop, new Stop("addr2")]);
-
-      return expect(stops.destination.value).to.deep.equal(originStop);
-    })
-
-    it("should return the first stop if no origin or destination modifier exists", () => {
-      const firstStop = new Stop("firstStop");
-      const stops = new Stops([firstStop, new Stop("addr1"), new Stop("addr2")]);
-
-      return expect(stops.destination.value).to.deep.equal(firstStop);
-    })
+    expect(stops.stops.value).to.be.an("array");
+    stops.stops.value.forEach((stop, index) => expect(stop).to.be.an.instanceOf(Stop).that.has.property("address", expectedAddresses[index]));
+    done();
   })
 
-  describe("method hasModifier", () => {
-    const originStop = new Stop("type:origin;address");
+  it("should add an index modifier to each stop", done => {
+    const props = { stops: "address1|address2" };
+    const expectedIndexes = ["0", "1"];
 
-    it("should return true if modifier is present", () => {
-      const stops = new Stops([new Stop("addr1"), originStop, new Stop("addr2")]);
+    const stops = new Stops(props);
 
-      return expect(stops.hasModifier("type", "origin")).to.be.true;
-    })
+    stops.stops.value.forEach((stop, index) => expect(stop.modifiers).to.have.property("index", expectedIndexes[index]));
+    done();
+  })
 
-    it("should return false if modifier isnt present", () => {
-      const stops = new Stops([new Stop("addr1"), new Stop("addr2"), new Stop("addr3")]);
+  it("stops should have expected value when no stops are sent", done => {
+    const stops = new Stops();
 
-      return expect(stops.hasModifier("type", "origin")).to.be.false;
-    })
+    expect(stops.stops.value).to.be.an("array").of.length(0);
+    expect(stops.stops.string).to.be.a("string").that.equals("");
+    expect(stops.stops.modifierString).to.be.a("string").that.equals("");
+    done();
+  })
+
+  it("stops should have expected value when one stop is sent", done => {
+    const stops = new Stops({ stops: "address" });
+
+    expect(stops.stops.value).to.be.an("array").of.length(1);
+    expect(stops.stops.string).to.be.a("string").that.equals("address");
+    expect(stops.stops.modifierString).to.be.a("string").that.equals("index:0;address");
+    done();
+  })
+
+  it("stops should have expected value when multiple stops are sent", done => {
+    const stops = new Stops({ stops: "address1|address2" });
+
+    expect(stops.stops.value).to.be.an("array").of.length(2);
+    expect(stops.stops.string).to.be.a("string").that.equals("address1|address2");
+    expect(stops.stops.modifierString).to.be.a("string").that.equals("index:0;address1|index:1;address2");
+    done();
+  })
+
+  it("origin should have expected value when no stops are sent", () => {
+    const stops = new Stops();
+
+    return expect(stops.origin).to.be.undefined;
+  })
+
+  it("origin should have expected value when origin is sent", done => {
+    const stops = new Stops({ stops: "type:origin;address" });
+
+    expect(stops.origin.value).to.be.an("object").with.property("address", "address");
+    expect(stops.origin.string).to.be.a("string").that.equals("address");
+    expect(stops.origin.modifierString).to.be.a("string").that.equals("type:origin;index:0;address");
+    done();
+  })
+
+  it("origin should have expected value when only destination is sent", done => {
+    const stops = new Stops({ stops: "type:destination;address" });
+
+    expect(stops.origin.value).to.be.an("object").with.property("address", "address");
+    expect(stops.origin.string).to.be.a("string").that.equals("address");
+    expect(stops.origin.modifierString).to.be.a("string").that.equals("type:destination;index:0;address");
+    done();
+  })
+
+  it("origin should have expected value when no origin or destination is sent", done => {
+    const stops = new Stops({ stops: "address" });
+
+    expect(stops.origin.value).to.be.an("object").with.property("address", "address");
+    expect(stops.origin.string).to.be.a("string").that.equals("address");
+    expect(stops.origin.modifierString).to.be.a("string").that.equals("index:0;address");
+    done();
+  })
+
+  it("destination should have expected value when no stops are sent", () => {
+    const stops = new Stops();
+
+    return expect(stops.destination).to.be.undefined;
+  })
+
+  it("destination should have expected value when destination is sent", done => {
+    const stops = new Stops({ stops: "type:destination;address" });
+
+    expect(stops.destination.value).to.be.an("object").with.property("address", "address");
+    expect(stops.destination.string).to.be.a("string").that.equals("address");
+    expect(stops.destination.modifierString).to.be.a("string").that.equals("type:destination;index:0;address");
+    done();
+  })
+
+  it("destination should have expected value when only origin is sent", done => {
+    const stops = new Stops({ stops: "type:origin;address" });
+
+    expect(stops.destination.value).to.be.an("object").with.property("address", "address");
+    expect(stops.destination.string).to.be.a("string").that.equals("address");
+    expect(stops.destination.modifierString).to.be.a("string").that.equals("type:origin;index:0;address");
+    done();
+  })
+
+  it("destination should have expected value when no origin or destination is sent", done => {
+    const stops = new Stops({ stops: "address" });
+
+    expect(stops.destination.value).to.be.an("object").with.property("address", "address");
+    expect(stops.destination.string).to.be.a("string").that.equals("address");
+    expect(stops.destination.modifierString).to.be.a("string").that.equals("index:0;address");
+    done();
+  })
+
+  it("waypoints should have expected value when no stops are sent", done => {
+    const stops = new Stops();
+
+    expect(stops.waypoints.value).to.be.an("array").of.length(0);
+    expect(stops.waypoints.string).to.be.a("string").that.equals("");
+    expect(stops.waypoints.modifierString).to.be.a("string").that.equals("");
+    done();
+  })
+
+  it("waypoints should have expected value when one stop is sent", done => {
+    const stops = new Stops({ stops: "address" });
+
+    expect(stops.waypoints.value).to.be.an("array").of.length(0);
+    expect(stops.waypoints.string).to.be.a("string").that.equals("");
+    expect(stops.waypoints.modifierString).to.be.a("string").that.equals("");
+    done();
+  })
+
+  it("waypoints should have expected value when two stops are sent", done => {
+    const stops = new Stops({ stops: "address1|address2" });
+
+    expect(stops.waypoints.value).to.be.an("array").of.length(1);
+    expect(stops.waypoints.string).to.be.a("string").that.equals("address2");
+    expect(stops.waypoints.modifierString).to.be.a("string").that.equals("index:1;address2");
+    done();
+  })
+
+  it("waypoints should have expected value when three or more stops are sent", done => {
+    const stops = new Stops({ stops: "address1|address2|address3" });
+
+    expect(stops.waypoints.value).to.be.an("array").of.length(2);
+    expect(stops.waypoints.string).to.be.a("string").that.equals("optimize:true|address2|address3");
+    expect(stops.waypoints.modifierString).to.be.a("string").that.equals("optimize:true|index:1;address2|index:2;address3");
+    done();
+  })
+
+  it("waypoints should have not have stops that are specified as the origin or destination", done => {
+    const stops = new Stops({ stops: "address1|type:origin;address2|address3|type:destination;address4" });
+
+    expect(stops.waypoints.value).to.be.an("array").of.length(2);
+    expect(stops.waypoints.string).to.be.a("string").that.equals("optimize:true|address1|address3");
+    expect(stops.waypoints.modifierString).to.be.a("string").that.equals("optimize:true|index:0;address1|index:2;address3");
+    done();
+  })
+
+  it("findByModifier should return undefined when modifier is not present", () => {
+    const res = new Stops()
+      .findByModifier("type");
+
+    return expect(res).to.be.undefined;
+  })
+
+  it("findByModifier should return the stop when modifier is present", () => {
+    const res = new Stops({ stops: "address1|type:origin;address2" })
+      .findByModifier("type");
+
+    return expect(res).to.be.an("object").with.property("address", "address2");
+  })
+
+  it("findByModifier should return undefined when modifier is present but value is not correct", () => {
+    const res = new Stops({ stops: "address1|type:origin;address2" })
+      .findByModifier("type", "destination");
+
+    return expect(res).to.be.undefined;
+  })
+
+  it("findByModifier should return the stop when modifier is present and value is correct", () => {
+    const res = new Stops({ stops: "address1|type:origin;address2" })
+      .findByModifier("type", "origin");
+
+      return expect(res).to.be.an("object").with.property("address", "address2");
+  })
+
+  it("hasModifier should return false when modifier is not present", () => {
+    const res = new Stops()
+      .hasModifier("type");
+
+    return expect(res).to.be.false;
+  })
+
+  it("hasModifier should return true when modifier is present", () => {
+    const res = new Stops({ stops: "address1|type:origin;address2" })
+      .hasModifier("type");
+
+    return expect(res).to.be.true;
+  })
+
+  it("hasModifier should return false when modifier is present but value is not correct", () => {
+    const res = new Stops({ stops: "address1|type:origin;address2" })
+      .hasModifier("type", "destination");
+
+    return expect(res).to.be.false;
+  })
+
+  it("hasModifier should return true when modifier is present and value is correct", () => {
+    const res = new Stops({ stops: "address1|type:origin;address2" })
+      .hasModifier("type", "origin");
+
+    return expect(res).to.be.true;
   })
 })
