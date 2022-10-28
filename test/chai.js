18c1b80d084eb20b41ebe9f5d0078893242ac5f2
stephen-j-oleary <s.james.oleary@gmail.com>
2022-10-27 18:42:08 -0600

    New chai json schema validator

--- test/chai.js
+++ test/chai.js
@@ -3,12 +3,13 @@ import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import sinonChai from "sinon-chai";
 import chaiHttp from "chai-http";
-import chaiJsonSchema from "chai-json-schema";
+import chaiJsonSchema from "chai-json-schema-ajv";
+import config from "../schemas/config.js";
 
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 chai.use(chaiHttp);
-chai.use(chaiJsonSchema);
+chai.use(chaiJsonSchema.create(config));
 
 export const { expect } = chai;
 
