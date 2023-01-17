dd5aecdcc24fa1e81bdd0a19332e0d6a21fed76a
stephen-j-oleary <s.james.oleary@gmail.com>
2023-01-16 21:48:18 -0700

    Add distance and text match properties

--- services/Location.js
+++ services/Location.js
@@ -1,4 +1,5 @@
 
+import _ from "lodash";
 import Address from "./Address.js";
 
 export default class Location {
@@ -19,6 +20,25 @@ export default class Location {
       return this;
     }
 
+    setDistance(val) {
+      this.distance = val;
+      return this;
+    }
+
+    setText({ full, main, secondary } = {}) {
+      this.full_text = full;
+      this.main_text = main;
+      this.secondary_text = secondary;
+      return this;
+    }
+
+    setMatch({ full, main, secondary } = {}) {
+      this.full_text_match = full;
+      this.main_text_match = main;
+      this.secondary_text_match = secondary;
+      return this;
+    }
+
     build() {
       return new Location(this);
     }
@@ -26,10 +46,9 @@ export default class Location {
 
   constructor(props = {}) {
     const { address } = props;
-
-    this.id = props.id;
-    this.lat = props.lat;
-    this.lng = props.lng;
     this.address = (address instanceof Address) ? address : new Address(address);
+
+    const ALLOWED_PROPS = ["id", "lat", "lng", "distance", "full_text", "full_text_match", "main_text", "main_text_match", "secondary_text", "secondary_text_match"];
+    Object.assign(this, _.pick(props, ALLOWED_PROPS));
   }
 }
