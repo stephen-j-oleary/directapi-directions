e8afaba042c0b77f53703b580df478b13905e6b9
stephen-j-oleary <s.james.oleary@gmail.com>
2023-01-16 20:50:21 -0700

    Refactor contructor to be less verbose

--- services/Request.js
+++ services/Request.js
@@ -1,14 +1,9 @@
 
+import _ from "lodash";
+
 export default class Request {
-  constructor({
-    query = {},
-    params = {},
-    body = {},
-    headers = {}
-  } = {}) {
-    this.query = query;
-    this.params = params;
-    this.body = body;
-    this.headers = headers;
+  constructor(props = {}) {
+    const ALLOWED_PROPS = ["query", "params", "body", "headers"];
+    Object.assign(this, _.pick(props, ALLOWED_PROPS));
   }
 }
