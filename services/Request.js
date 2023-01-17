2042720c07190db53bb91f3156e09fd6384b91cf
stephen-j-oleary <s.james.oleary@gmail.com>
2023-01-16 20:52:01 -0700

    Add static create method

--- services/Request.js
+++ services/Request.js
@@ -2,6 +2,10 @@
 import _ from "lodash";
 
 export default class Request {
+  static create(props = {}) {
+    return new Request(props);
+  }
+
   constructor(props = {}) {
     const ALLOWED_PROPS = ["query", "params", "body", "headers"];
     Object.assign(this, _.pick(props, ALLOWED_PROPS));
